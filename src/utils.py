9277c94221ec22e0bbd1ebbc5206c34c702cd2a1
Kaushik6 <barikkaushik48@gmail.com>
2025-12-26 12:57:54 +0530

    Hyperparameter Tuining

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,10 @@ import pandas as pd
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from catboost import CatBoostRegressor
+
+##CatBoostRegressor is not fully compatible with sklearn’s GridSearchCV because it does not implement sklearn’s internal estimator tags, so hyperparameter tuning must be handled separately.
 
 def save_object(file_path, obj):
     try:
@@ -21,13 +25,28 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report={}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            para=param.get(model_name,{})
 
+            if model_name == "CatBoost Regressor":
+                CB_model=CatBoostRegressor(**para,verbose=False)
+                CB_model.fit(X_train,y_train)
+                y_train_pred = CB_model.predict(X_train)
+                y_test_pred = CB_model.predict(X_test)
+                train_model_score = r2_score(y_train,y_train_pred)
+                test_model_score = r2_score(y_test,y_test_pred)
+                report[model_name] = test_model_score
+                continue
+
+            if para:
+                gs = GridSearchCV(model,para,cv=3)
+                gs.fit(X_train,y_train)
+                model.set_params(**gs.best_params_)
+            
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -38,7 +57,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
             test_model_score = r2_score(y_test,y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
         
         return report
     except Exception as e:
